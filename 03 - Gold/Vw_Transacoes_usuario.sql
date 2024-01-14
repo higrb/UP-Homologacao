20051d73681b4f3c98c2f7c47a73719cebb828be
higrb <higrb>
2024-01-14 11:53:13 +0000

    Versão 1 da view.

---
+++ 03 - Gold/Vw_Transacoes_usuario.sql
@@ -0,0 +1,14 @@
+-- Databricks notebook source
+CREATE OR REPLACE VIEW GOLDDEV.DEFAULT.transacoes_usuarios AS
+SELECT FTO.* FROM silverdev.default.f_trn_usu fto
+INNER JOIN silverdev.default.dm_cli cli ON cli.SRK_CLI = fto.SRK_CLI
+INNER JOIN silverdev.default.dm_est est ON est.SRK_ESTAB = fto.SRK_ESTAB
+INNER JOIN silverdev.default.dm_tip_prd prd ON prd.SRK_TIP_PRD = fto.SRK_TIP_PRD
+INNER JOIN silverdev.default.dm_usu usu ON usu.cod_prp_pf = fto.COD_PRP_PF
+INNER JOIN silverdev.default.dm_pod mov ON mov.SRK_DAT = fto.SRK_DAT_MOV_AUT;
+
+-- COMMAND ----------
+
+-- MAGIC %sql
+-- MAGIC select * from silverdev.default.dm_cli
+-- MAGIC where COD_ENT = '257662'
